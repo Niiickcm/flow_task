54f36d157acd8987bbb8fa6fa16a1b84b313edc3
Niiickcm <thiagomiguel.05@hotmail.com>
2024-02-10 14:32:06 -0300

    fix bugs

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,16 +1,13 @@
 import { Toaster } from "sonner";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ptBR } from "@clerk/localizations";
 
 import { ModalProvider } from "@/components/providers/modal-provider";
 import { QueryProvider } from "@/components/providers/query-provider";
 
-const PlatformLayout = ({
-  children
-}: {
-  children: React.ReactNode;
-}) => {
+const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ClerkProvider>
+    <ClerkProvider localization={ptBR}>
       <QueryProvider>
         <Toaster />
         <ModalProvider />
